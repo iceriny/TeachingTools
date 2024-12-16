0fd5e9be859b652ce7e087afdd79abc28346f5ef
iceriny <suqianchengice@gmail.com>
2024-12-16 16:42:35 +0800

    feat(QRGenerator): 添加二维码解析功能
    - 集成 jsQR 库以实现二维码解析
    - 新增 QRParse 组件用于解析二维码图片内容
    - 优化二维码生成器界面布局，增加新功能区域
    - 添加自定义图标大小功能
    - 新增第三方二维码内容解析功能
    - 重构代码以提高可维护性

--- src/components/Page/QRGenerator/QRParse.tsx
+++ src/components/Page/QRGenerator/QRParse.tsx
@@ -1,17 +1,15 @@
-import { Card, Divider, Input, Space } from "antd";
+import { Divider, Input, Space } from "antd";
 import InputFile from "../../InputFile";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import jsQR from "jsqr";
 
 interface Props {
     onChange?: (content: string) => void;
 }
-function QRParse({ onChange: handleCopyContent }: Props) {
+function QRParse({ onChange }: Props) {
     const inputRef = useRef<HTMLTextAreaElement>(null);
-    const handleTextAreaChange = (
-        e: React.ChangeEvent<HTMLTextAreaElement>
-    ) => {
-        handleCopyContent?.(e.target.value);
-    };
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [qrContent, setQRContent] = useState<string>();
     return (
         <>
             <Space
@@ -21,9 +19,113 @@ function QRParse({ onChange: handleCopyContent }: Props) {
             >
                 <InputFile
                     title="请选择二维码图片"
-                    onChange={(fileContent) => {
-                        inputRef.current &&
-                            (inputRef.current.value = fileContent);
+                    onChange={(_, fileContent) => {
+                        const img = new Image();
+                        img.src = fileContent;
+                        img.onload = () => {
+                            const canvas = canvasRef.current;
+                            if (!canvas) return;
+                            const ctx = canvas.getContext("2d")!;
+                            canvas.width = img.width;
+                            canvas.height = img.height;
+                            // 先在 Canvas 上绘制一个背景色
+                            ctx.fillStyle = "#ffcc00";
+                            ctx.fillRect(0, 0, canvas.width, canvas.height);
+                            // 将图片绘制到 Canvas 上
+                            ctx.drawImage(img, 0, 0, img.width, img.height);
+
+                            // 获取 ImageData
+                            let imageData = ctx.getImageData(
+                                0,
+                                0,
+                                img.width,
+                                img.height
+                            );
+                            // 使用jsQR解码
+                            let qrData = jsQR(
+                                imageData.data,
+                                imageData.width,
+                                imageData.height,
+                                { inversionAttempts: "attemptBoth" }
+                            );
+
+                            // 如果为解析成功, 计算平均亮度, 先进行搞对比填充背景色, 然后再次解码
+                            if (!qrData) {
+                                // 清空整个画布
+                                ctx.clearRect(
+                                    0,
+                                    0,
+                                    canvas.width,
+                                    canvas.height
+                                );
+                                ctx.drawImage(img, 0, 0, img.width, img.height);
+                                // 获取像素数据
+                                imageData = ctx.getImageData(
+                                    0,
+                                    0,
+                                    canvas.width,
+                                    canvas.height
+                                );
+                                const data = imageData.data;
+
+                                let totalBrightness = 0;
+                                let pixelCount = 0;
+
+                                // 遍历像素数据
+                                for (let i = 0; i < data.length; i += 4) {
+                                    const r = data[i]; // 红色通道
+                                    const g = data[i + 1]; // 绿色通道
+                                    const b = data[i + 2]; // 蓝色通道
+                                    const a = data[i + 3]; // 透明度通道
+
+                                    if (a > 0) {
+                                        // 忽略完全透明像素
+                                        const brightness =
+                                            0.299 * r + 0.587 * g + 0.114 * b;
+                                        totalBrightness += brightness;
+                                        pixelCount++;
+                                    }
+                                }
+
+                                // 计算平均亮度
+                                const averageBrightness =
+                                    pixelCount > 0
+                                        ? totalBrightness / pixelCount
+                                        : 0;
+                                if (averageBrightness < 0.5) {
+                                    ctx.fillStyle = "#ff00dc";
+                                } else {
+                                    ctx.fillStyle = "#000000";
+                                }
+                                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                                // 再次解码
+                                // 将图片绘制到 Canvas 上
+                                ctx.drawImage(img, 0, 0, img.width, img.height);
+
+                                // 获取 ImageData
+                                imageData = ctx.getImageData(
+                                    0,
+                                    0,
+                                    img.width,
+                                    img.height
+                                );
+                                // 使用jsQR解码
+                                qrData = jsQR(
+                                    imageData.data,
+                                    imageData.width,
+                                    imageData.height,
+                                    { inversionAttempts: "attemptBoth" }
+                                );
+                            }
+                            console.log(qrData);
+                            const result =
+                                qrData?.data || "[二维码解析内容为空]";
+                            inputRef.current &&
+                                (inputRef.current.value = result);
+
+                            setQRContent(result);
+                            onChange?.(result);
+                        };
                     }}
                 />
             </Space>
@@ -31,8 +133,12 @@ function QRParse({ onChange: handleCopyContent }: Props) {
             <Input.TextArea
                 ref={inputRef}
                 rows={4}
-                onChange={handleTextAreaChange}
+                placeholder="解析结果"
+                readOnly
+                value={qrContent}
+                // onChange={handleTextAreaChange}
             />
+            <canvas ref={canvasRef} style={{ display: "none" }} />
         </>
     );
 }
